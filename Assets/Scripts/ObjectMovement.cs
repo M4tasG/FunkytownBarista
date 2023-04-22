d61bd0a2f6af245e3cd91ddc9e649ea0294e0f3b
Matas Grybauskas <79117034+M4tasG@users.noreply.github.com>
2023-04-22 21:31:52 +0300

    removed debug log from ObjectMovement script

--- Assets/Scripts/ObjectMovement.cs
+++ Assets/Scripts/ObjectMovement.cs
@@ -10,21 +10,18 @@ public class ObjectMovement : MonoBehaviour
     {
         _zCoord = Camera.main.WorldToScreenPoint(gameObject.transform.position).z;
         _offset = gameObject.transform.position - GetMouseWorldPos();
-        Debug.Log("Mouse down");
     }
 
     private Vector3 GetMouseWorldPos()
     {
         var mousePoint = Input.mousePosition;
         mousePoint.z = _zCoord;
-
-        Debug.Log("Getting mouse pos");
+        
         return Camera.main.ScreenToWorldPoint(mousePoint);
     }
 
     private void OnMouseDrag()
     {
         transform.position = GetMouseWorldPos() + _offset;
-        Debug.Log("Dragging mouse");
     }
 }
