126fff7a8baff291492578a9cd24d94ee29779ce
Matas Grybauskas <79117034+M4tasG@users.noreply.github.com>
2023-04-23 13:48:03 +0300

    Adjusted item spawning relative to spawner position

--- Assets/Scripts/ItemSpawner.cs
+++ Assets/Scripts/ItemSpawner.cs
@@ -6,8 +6,16 @@ using UnityEngine;
 public class ItemSpawner : MonoBehaviour
 {
     public Item.ItemType itemName;
+    private Vector3 spawnPosition;
+
+    private void Awake()
+    {
+        Vector3 currentPosition = gameObject.transform.position;
+        spawnPosition = new Vector3(currentPosition.x, currentPosition.y - 1);
+    }
+
     private void OnMouseDown()
     {
-        ItemWorld.SpawnItemWorld(new Vector3(0, 0), new Item { itemType = itemName, amount = 1 });
+        ItemWorld.SpawnItemWorld(spawnPosition, new Item { itemType = itemName, amount = 1 });
     }
 }
