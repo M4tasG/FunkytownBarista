5ab95da7e0a42bbb9543f91f2dc7ff5482fa65b5
Matas Grybauskas <79117034+M4tasG@users.noreply.github.com>
2023-04-23 13:42:30 +0300

    removed test item spawns

--- Assets/Scripts/CupController.cs
+++ Assets/Scripts/CupController.cs
@@ -10,10 +10,7 @@ public class CupController : MonoBehaviour
     {
         Debug.Log("Awaking CupController");
         inventory = new Inventory();
-
-        ItemWorld.SpawnItemWorld(new Vector3(0, 0), new Item { itemType = Item.ItemType.Sugar, amount = 1 });
-        ItemWorld.SpawnItemWorld(new Vector3(1, 1), new Item { itemType = Item.ItemType.Coffee, amount = 1 });
-        ItemWorld.SpawnItemWorld(new Vector3(-2, -1), new Item { itemType = Item.ItemType.Sugar, amount = 1 });
+        
     }
 
     private void OnTriggerEnter2D(Collider2D col)
