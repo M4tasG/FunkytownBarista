2e09cbe3299e833fdc652197d072c9e85f6ec4f0
Matas Grybauskas <79117034+M4tasG@users.noreply.github.com>
2023-04-23 13:20:09 +0300

    added item world script to handle item spawning

---
+++ Assets/Scripts/ItemWorld.cs
@@ -0,0 +1,51 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemWorld : MonoBehaviour
+{
+
+    public Transform pfItemWorld;
+    public static ItemWorld SpawnItemWorld(Vector3 position, Item item)
+    {
+        //Debug.Log(position);
+        //Debug.Log(item);
+        Debug.Log(ItemAssets.Instance);
+        Debug.Log(ItemAssets.Instance.pfItemWorld);
+        Transform transform = Instantiate(ItemAssets.Instance.pfItemWorld, position, Quaternion.identity);
+
+        Debug.Log(transform);
+        ItemWorld itemWorld = transform.GetComponent<ItemWorld>();
+        itemWorld.SetItem(item);
+
+        return itemWorld;
+    }
+    
+    
+    private Item item;
+    private SpriteRenderer spriteRenderer;
+
+    private void Awake()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    public void SetItem(Item item)
+    {
+        this.item = item;
+        // Not implemented yet
+        // https://www.youtube.com/watch?v=2WnAOV7nHW0
+        // spriteRenderer.sprite = item.GetSprite()
+    }
+
+    public Item GetItem()
+    {
+        return item;
+    }
+
+    public void DestroySelf()
+    {
+        Destroy(gameObject);
+    }
+}
