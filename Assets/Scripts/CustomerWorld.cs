f38fc00085a09d9403a30bbd340eebe285fd9515
Matas Grybauskas <79117034+M4tasG@users.noreply.github.com>
2023-04-26 17:31:01 +0300

    added an order variable to the class

--- Assets/Scripts/CustomerWorld.cs
+++ Assets/Scripts/CustomerWorld.cs
@@ -1,18 +1,16 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class CustomerWorld : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public ActiveOrder CustomerCoffeeOrder;
 
-    // Update is called once per frame
-    void Update()
+    private void Awake()
     {
-        
+        CustomerCoffeeOrder = new ActiveOrder();
+        Debug.Log(CustomerCoffeeOrder.CoffeeName);
+        Debug.Log(CustomerCoffeeOrder.CurrentOrder);
     }
 }
