0930973a3330a5653cf3c62272531eeba9222f07
Matas Grybauskas <79117034+M4tasG@users.noreply.github.com>
2023-04-26 21:06:17 +0300

    implemented Order check and delivery zone

---
+++ Assets/Scripts/OrderDelivery.cs
@@ -0,0 +1,46 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class OrderDelivery : MonoBehaviour
+{
+    public static ActiveOrder requiredOrder;
+    private void OnTriggerEnter2D(Collider2D col)
+    {
+        Debug.Log("Collider " + col + " entered the delivery area");
+        Inventory cupInventory = col.GetComponent<CupController>().GetInventory();
+        List<Item> cupIngredients = cupInventory.GetItemList();
+
+        
+        if (MatchIngrediets(cupIngredients, requiredOrder.CurrentOrder))
+        {
+            Debug.Log("Match!!!!!!");
+        }
+        else
+        {
+            Debug.Log("Does not match...");
+        }
+    }
+
+    private bool MatchIngrediets(List<Item> cupContents, List<Item> requiredContents)
+    {
+        bool isEqual = true;
+        var counter = 0;
+        foreach (var item in cupContents)
+        {
+            if (item.itemType != requiredContents[counter].itemType || item.amount != requiredContents[counter].amount)
+            {
+                isEqual = false;
+            }
+            counter = counter + 1;
+        }
+
+        return isEqual;
+    }
+    public static void SetOrder(ActiveOrder order)
+    {
+        requiredOrder = order;
+    }
+}
