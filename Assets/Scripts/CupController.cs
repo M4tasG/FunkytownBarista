f779c3be4171504aebb20d04c89078e4ec781279
Matas Grybauskas <79117034+M4tasG@users.noreply.github.com>
2023-04-26 21:06:34 +0300

    added GetInventory of Cup

--- Assets/Scripts/CupController.cs
+++ Assets/Scripts/CupController.cs
@@ -16,14 +16,17 @@ public class CupController : MonoBehaviour
     private void OnTriggerEnter2D(Collider2D col)
     {
         ItemWorld itemWorld = col.GetComponent<ItemWorld>();
+
         if (itemWorld != null)
         {
             inventory.AddItem(itemWorld.GetItem());
             itemWorld.DestroySelf();
-            Debug.Log(inventory.GetItemCount());
-            Debug.Log(inventory.GetItemList()[0].itemType);
-            Debug.Log(inventory.GetItemList()[0].amount);
         }
+    }
+
+    public Inventory GetInventory()
+    {
+        return inventory;
     }
     
 }
