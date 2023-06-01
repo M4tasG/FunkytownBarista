017af3187efd6b735e8da5d93e323634f1ad7721
Matas Grybauskas <79117034+M4tasG@users.noreply.github.com>
2023-06-01 13:09:49 +0300

    added a summary screen at the end of the level

--- Assets/Scripts/LevelEndHandler.cs
+++ Assets/Scripts/LevelEndHandler.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEditor.SceneManagement;
 using UnityEngine;
 using UnityEngine.SceneManagement;
@@ -12,15 +13,41 @@ public class LevelEndHandler : MonoBehaviour
     public GameObject gameWonText;
     public GameObject gameLostText;
 
+    public GameObject levelEndScreen;
+    public GameObject levelEndText;
+
     public int CoinsRequired;
     
     public float levelTimer;
     private void Awake()
     {
-        Invoke("SwitchLevel", levelTimer);
+        Invoke("EndLevel", levelTimer);
     }
 
-    void SwitchLevel()
+    void EndLevel()
+    {
+        if (SceneManager.GetActiveScene().name == "Level1")
+        {
+            levelEndScreenFunc(CoinController.Instance.Coins);
+        }
+        else if (SceneManager.GetActiveScene().name == "Level2")
+        {
+            levelEndScreenFunc(CoinController.Instance.Coins);
+        }
+        else if (SceneManager.GetActiveScene().name == "Level3")
+        {
+            isGameWon(CoinController.Instance.Coins);
+        }
+    }
+
+    public void levelEndScreenFunc(int coinsGathered)
+    {
+        levelEndScreen.SetActive(true);
+        levelEndText.GetComponent<TMP_Text>().text = "Level over! Coins collected: " + coinsGathered;
+        levelEndText.SetActive(true);
+    }
+
+    public void switchToNextLevel()
     {
         if (SceneManager.GetActiveScene().name == "Level1")
         {
@@ -30,11 +57,9 @@ public class LevelEndHandler : MonoBehaviour
         {
             SceneManager.LoadScene("Level3");
         }
-        else if (SceneManager.GetActiveScene().name == "Level3")
-        {
-            isGameWon(CoinController.Instance.Coins);
-        }
     }
+    
+    
     public void isGameWon(int coinsGathered)
     {
         gameOverScreen.SetActive(false);
